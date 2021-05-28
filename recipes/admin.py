d905b096493974ad01cf9a5e36f9ed026e382445
pivannikov <pivannikov@yandex.ru>
2021-05-28 21:34:08 +0200

    init

---
+++ recipes/admin.py
@@ -0,0 +1,65 @@
+from django.contrib import admin
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.utils.safestring import mark_safe
+
+from .models import *
+
+
+class PostAdminForm(forms.ModelForm):
+    ingredients = forms.CharField(label='Ингредиенты', widget=CKEditorUploadingWidget(), required=False)
+    content = forms.CharField(label='Описание', widget=CKEditorUploadingWidget(), required=False)
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostImageAdmin(admin.ModelAdmin):
+    pass
+
+
+class PostImageInline(admin.StackedInline):
+    model = PostImage
+    max_num = 10
+    extra = 0
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    form = PostAdminForm
+    save_as = True
+    list_display = ('id', 'title', 'created_at', 'is_published', 'favorites', 'cooked', 'category', 'views', 'get_photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title')
+    list_editable = ('is_published', 'favorites', 'cooked')
+    list_filter = ('is_published', 'category', 'tags')
+    inlines = [PostImageInline, ]
+    save_on_top = True
+    readonly_fields = ('created_at', 'views')
+    fields = ('title', 'slug', 'category', 'tags', 'favorites', 'cooked', 'ingredients', 'content', 'main_photo', 'is_published', 'views', 'created_at')
+
+    def get_photo(self, obj):
+        if obj.main_photo:
+            return mark_safe(f'<img src="{obj.main_photo.url}" width="50">')
+        return '-'
+
+    get_photo.short_description = 'Main photo'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ('title', 'parent')
+    list_display = ('title', 'parent')
+    list_filter = ('parent',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ('title',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(PostImage, PostImageAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
---
+++ recipes/models.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, verbose_name='Url')
+    parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name_plural = 'Categories'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    ingredients = models.TextField(blank=True)
+    content = models.TextField(blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    main_photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    favorites = models.BooleanField(default=False)
+    cooked = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
+    views = models.IntegerField(default=0, verbose_name='Views')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-id']
+
+
+class PostImage(models.Model):
+    image_path = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+
+    def __str__(self):
+        return self.post.title
+
+    class Meta:
+        verbose_name = 'Images gallery'
+        verbose_name_plural = 'Additional images'
